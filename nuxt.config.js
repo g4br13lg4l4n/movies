f3cf5ee8cdc46ec4529b2e3d26d228ec4a37d737
Gabriel Galan <gabriel.galan92@gmail.com>
2019-06-08 19:43:43 -0500

    chage host prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,7 @@ module.exports = {
 
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: "http://localhost:3001/api/v1/"
+    baseURL: "http://206.189.169.235:3001/api/v1/"
   },
 
   server: {
