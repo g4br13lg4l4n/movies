877ce4acfce65a3e683c20a7b081db6ed0e22bcb
Gabriel Galan <gabriel.galan92@gmail.com>
2019-06-08 19:02:44 -0500

    chage host prod

--- plugins/api.js
+++ plugins/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api/v1/' : 'http://206.189.169.235/api/v1/'
+  baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3001/api/v1/' : 'http://206.189.169.235:3001/api/v1/'
 })
 
  export default api
