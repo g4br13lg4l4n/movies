9a3d3146a141f6e0ed5a5cce2e81a31561bd9a59
Gabriel Galan <gabriel.galan92@gmail.com>
2019-07-02 18:23:16 -0500

    add https fix 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ module.exports = {
 
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: "https://digitalfilms.us:3001/api/v1/"
+    baseURL: "http://digitalfilms.us:3001/api/v1/"
     //baseURL: "http://localhost:3001/api/v1/"
   },
 
