b2c2b37c4c6082833afe7177b6522cc074c287dd
Gabriel Galan <gabriel.galan92@gmail.com>
2019-07-02 16:24:54 -0500

    test 4

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,13 +84,13 @@ module.exports = {
 
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: "http://206.189.169.235:3001/api/v1/"
+    baseURL: "http://http://digitalfilms.us:3001/api/v1/"
     //baseURL: "http://localhost:3001/api/v1/"
   },
 
   server: {
     prod: {
-      host: '206.189.169.235',
+      host: 'http://digitalfilms.us',
       port: 3001
     },
     dev: {
