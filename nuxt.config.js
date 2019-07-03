d67a58f5017d9d6ec8e7a2d43b01d5b00e99b38b
Gabriel Galan <gabriel.galan92@gmail.com>
2019-07-02 21:05:16 -0500

    update head 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'keywords', content: 'peliculas, series, gratis, anime, ver'},
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
